8aa839e5331ad95532bf490e0a9de58421346e84
Alan Paiva <jpaiva.alan@gmail.com>
2021-10-31 21:01:57 -0300

    Use multiple covariance matrixes

--- bayes/bayes.py
+++ bayes/bayes.py
@@ -3,42 +3,46 @@ import numpy as np
 
 class GaussianBayes:
     def __init__(self):
-        self.all_classes = None
+        self.num_classes = None
         self.means = None
         self.cov_matrix = None
+        self.priori = None
 
     def train(self, dataset):
-        self.all_classes = np.unique(dataset[:, -1]).astype(int)
+        self.num_classes = np.max(dataset[:, -1]).astype(int) + 1
+        self.generate_priori(dataset)
         self.generate_means(dataset)
-        self.generate_cov_matrix(dataset[:, :-1])
+        self.generate_cov_matrix(dataset)
+
+    def generate_priori(self, dataset):
+        p = list()
+        for c in range(self.num_classes):
+            s = np.sum(dataset[:, -1] == c)
+            p.append(s / dataset.shape[0])
+        self.priori = np.array(p)
 
     def generate_means(self, dataset):
-        self.means = np.array([])
-        for c in self.all_classes:
+        m = list()
+        for c in range(self.num_classes):
             points = dataset[dataset[:, -1] == c][:, :-1]
             mean = np.mean(points, axis=0)
-            self.means = np.append(self.means, mean).reshape((-1, points.shape[1]))
+            m.append(mean)
+        self.means = np.array(m)
 
-    def generate_cov_matrix(self, x):
-        num_patterns = x.shape[0]
-        num_attributes = x.shape[1]
-        means = np.mean(x, axis=0)
-        self.cov_matrix = np.zeros((num_attributes, num_attributes))
-        for i in range(0, num_attributes):
-            for j in range(0, num_attributes):
-                x_i, x_j = x[:, i], x[:, j]
-                self.cov_matrix[i][j] = np.sum((x_i - means[i]) * (x_j - means[j])) / (num_patterns - 1)
-
-        # Asserting correctness of the generated covariance matrix with the built in numpy implementation
-        assert np.unique(np.isclose(self.cov_matrix, np.cov(x.T))).item()
+    def generate_cov_matrix(self, dataset):
+        cm = list()
+        for c in range(self.num_classes):
+            xc = dataset[dataset[:, -1] == c][:, :-1]
+            cm.append(np.cov(xc, rowvar=False))
+        self.cov_matrix = np.array(cm)
 
     def predict(self, x):
         d = x.shape[0]
-        p_x = np.array([])
-        det_cov_matrix = np.linalg.det(self.cov_matrix)
-        for c in self.all_classes:
-            p = 1 / (np.power(2 * np.pi, d / 2) * np.power(det_cov_matrix, d / 2))
-            p *= np.exp(-0.5 * ((x - self.means[c]).reshape((1, d)) @ np.linalg.inv(self.cov_matrix) @ (x - self.means[c]).reshape(d, 1)).item())
-            p_x = np.append(p_x, p)
-        return np.argmax(p_x)
-
+        p_x = list()
+        for c in range(self.num_classes):
+            det_cov_matrix = np.linalg.det(self.cov_matrix[c])
+            p = 1 / (np.power(2 * np.pi, d / 2) * np.sqrt(det_cov_matrix))
+            p *= np.exp(-1 / 2 * ((x - self.means[c]).reshape((1, d)) @ np.linalg.inv(self.cov_matrix[c]) @ (x - self.means[c]).reshape(d, 1)).item())
+            p *= self.priori[c]
+            p_x.append(p)
+        return np.argmax(np.array(p_x))
